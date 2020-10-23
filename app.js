42247090a934ea016cb27e91f75bde68b7741b81
Anshul <anshulsaha15@gmail.com>
2020-10-23 22:24:13 +0530

    init

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const session = require('express-session');
+require('dotenv').config()
+
+const app = express();
+
+app.use(
+    session({
+      secret: 'secret',
+      resave: true,
+      saveUninitialized: true
+    })
+);
+
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: false }));
+app.use('/', require('./routes/index'))
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
