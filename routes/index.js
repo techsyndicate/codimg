1c2b1d0ea04a811472ebdcd99fb76a3c37909763
Anshul <anshulsaha15@gmail.com>
2020-10-24 20:39:09 +0530

    created new report page and results page

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,29 @@ const getLicense = require('../controllers/licenseDetector')
 
 router.get('/', (req, res) => {
     res.render('index')
-    getResult('https://github.com/jjwilly16/node-pdftk')
-    getLicense('https://github.com/jjwilly16/node-pdftk')
+    // getLicense('https://github.com/jjwilly16/node-pdftk')
+    // getResult('https://github.com/jjwilly16/node-pdftk')
+})
+
+router.get('/new', (req, res) => {
+    res.render('new', {
+        layout: false
+    })
+})
+
+router.post('/new', (req, res) => {
+    const { name, email, repoUrl } = req.body
+    res.render('results', {
+        name,
+        email,
+        repoUrl
+    })
+})
+
+router.get('/results', (req, res) => {
+    res.render('results', {
+        layout: false
+    })
 })
 
 module.exports = router;
